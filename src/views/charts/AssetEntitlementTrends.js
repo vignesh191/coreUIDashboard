34655cbfe95813d3f61bea7b9645ab966299caba
vignesh191 <70774793+vignesh191@users.noreply.github.com>
2020-10-05 15:34:08 -0700

    Add files via upload

---
+++ src/views/charts/AssetEntitlementTrends.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  CBadge,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CCallout
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import MainChartExample from '../charts/MainChartExample.js'
+
+
+function AssetEntitlementTrends(props) {
+
+return (
+      <CCard>
+        <CCardBody>
+          <CRow>
+            <CCol sm="5">
+              <h4 id="traffic" className="card-title mb-0">Assets and Entitlement Trends</h4>
+              <div className="small text-muted">September 2020</div>
+            </CCol>
+            <CCol sm="7" className="d-none d-md-block">
+              <CButton color="primary" className="float-right">
+                <CIcon name="cil-cloud-download"/>
+              </CButton>
+              <CButtonGroup className="float-right mr-3">
+                {
+                  ['Day', 'Month', 'Year'].map(value => (
+                    <CButton
+                      color="outline-secondary"
+                      key={value}
+                      className="mx-0"
+                      active={value === 'Month'}
+                    >
+                      {value}
+                    </CButton>
+                  ))
+                }
+              </CButtonGroup>
+            </CCol>
+          </CRow>
+          <MainChartExample style={{height: '300px', marginTop: '40px'}}/>
+        </CCardBody>
+        <CCardFooter>
+          <CRow className="text-center">
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">Active Policies</div>
+              <strong>28</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="success"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
+              <div className="text-muted">Policy Violation</div>
+              <strong>2</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="info"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">Policy Violation Exceptions</div>
+              <strong>245</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="warning"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0">
+              <div className="text-muted">New Users</div>
+              <strong>22</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                color="danger"
+                value={40}
+              />
+            </CCol>
+            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
+              <div className="text-muted">Terminated User Entitlements</div>
+              <strong>425</strong>
+              <CProgress
+                className="progress-xs mt-2"
+                precision={1}
+                value={40}
+              />
+            </CCol>
+          </CRow>
+        </CCardFooter>
+      </CCard>
+
+
+)
+
+}
+
+export default AssetEntitlementTrends;
